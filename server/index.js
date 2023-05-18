356efe8ceec58293626b6814f6501b8094fcb1c0
gnaghiyeva <gulnar.nagiyeva17@gmail.com>
2023-05-18 10:17:44 +0400

    connect mongo

--- server/index.js
+++ server/index.js
@@ -3,176 +3,194 @@ const app = express()
 const crypto = require("crypto")
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 app.use( bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 app.use(cors())
 
+
+
+const StudentsSchema = new mongoose.Schema({
+        name:String,
+        surname:String,
+        birthdate:String,
+        faculty:String,
+        occupation:String,
+        GPA:Number,
+        isMarried:Boolean
+})
+const StudentModel = mongoose.model('Students',StudentsSchema)
+
+mongoose.connect('mongodb+srv://gulnar_admin:Admin123@spotify.13fyfgh.mongodb.net/?retryWrites=true&w=majority')
+.then(()=>console.log('Mongodb connected'));
+
+
 // const Validation = require("./validation/StudentsScheme")
 const PORT = 5050
 
-const STUDENTS=[
-    {
-        id:1,
-        name:"Gulnar",
-        surname:"Naghiyeva",
-        birthdate:"22-09-2002",
-        faculty:"Applied Mathematic SABAH groups",
-        occupation:"Front end developer",
-        GPA:95,
-        isMarried:false
-    },
-    {
-        id:2,
-        name:"Nigar",
-        surname:"Naghiyeva",
-        birthdate:"17-05-2002",
-        faculty:"Chemistry",
-        occupation:"teacher",
-        GPA:95,
-        isMarried:false
-    },
-    {
-        id:3,
-        name:"Gulnar",
-        surname:"Naghiyeva",
-        birthdate:"22-09-2002",
-        faculty:"Applied Mathematic SABAH groups",
-        occupation:"Front end developer",
-        GPA:95,
-        isMarried:false
-    },
-    {
-        id:4,
-        name:"Nigar",
-        surname:"Naghiyeva",
-        birthdate:"17-05-2002",
-        faculty:"Chemistry",
-        occupation:"teacher",
-        GPA:95,
-        isMarried:false
-    },
-    {
-        id:5,
-        name:"Gulnar",
-        surname:"Naghiyeva",
-        birthdate:"22-09-2002",
-        faculty:"Applied Mathematic SABAH groups",
-        occupation:"Front end developer",
-        GPA:95,
-        isMarried:false
-    },
-    {
-        id:6,
-        name:"Nigar",
-        surname:"Naghiyeva",
-        birthdate:"17-05-2002",
-        faculty:"Chemistry",
-        occupation:"teacher",
-        GPA:95,
-        isMarried:false
-    },
-    {
-        id:7,
-        name:"Gulnar",
-        surname:"Naghiyeva",
-        birthdate:"22-09-2002",
-        faculty:"Applied Mathematic SABAH groups",
-        occupation:"Front end developer",
-        GPA:95,
-        isMarried:false
-    },
-    {
-        id:8,
-        name:"Nigar",
-        surname:"Naghiyeva",
-        birthdate:"17-05-2002",
-        faculty:"Chemistry",
-        occupation:"teacher",
-        GPA:95,
-        isMarried:false
-    },
-    {
-        id:9,
-        name:"Gulnar",
-        surname:"Naghiyeva",
-        birthdate:"22-09-2002",
-        faculty:"Applied Mathematic SABAH groups",
-        occupation:"Front end developer",
-        GPA:95,
-        isMarried:false
-    },
-    {
-        id:10,
-        name:"Nigar",
-        surname:"Naghiyeva",
-        birthdate:"17-05-2002",
-        faculty:"Chemistry",
-        occupation:"teacher",
-        GPA:95,
-        isMarried:false
-    },
-    {
-        id:11,
-        name:"Gulnar",
-        surname:"Naghiyeva",
-        birthdate:"22-09-2002",
-        faculty:"Applied Mathematic SABAH groups",
-        occupation:"Front end developer",
-        GPA:95,
-        isMarried:false
-    },
-    {
-        id:12,
-        name:"Nigar",
-        surname:"Naghiyeva",
-        birthdate:"17-05-2002",
-        faculty:"Chemistry",
-        occupation:"teacher",
-        GPA:95,
-        isMarried:false
-    },
-    {
-        id:13,
-        name:"Gulnar",
-        surname:"Naghiyeva",
-        birthdate:"22-09-2002",
-        faculty:"Applied Mathematic SABAH groups",
-        occupation:"Front end developer",
-        GPA:95,
-        isMarried:false
-    },
-    {
-        id:14,
-        name:"Nigar",
-        surname:"Naghiyeva",
-        birthdate:"17-05-2002",
-        faculty:"Chemistry",
-        occupation:"teacher",
-        GPA:95,
-        isMarried:false
-    },
-    {
-        id:15,
-        name:"Gulnar",
-        surname:"Naghiyeva",
-        birthdate:"22-09-2002",
-        faculty:"Applied Mathematic SABAH groups",
-        occupation:"Front end developer",
-        GPA:95,
-        isMarried:false
-    },
-    {
-        id:16,
-        name:"Nigar",
-        surname:"Naghiyeva",
-        birthdate:"17-05-2002",
-        faculty:"Chemistry",
-        occupation:"teacher",
-        GPA:95,
-        isMarried:false
-    },
+// const STUDENTS=[
+//     {
+//         id:1,
+//         name:"Gulnar",
+//         surname:"Naghiyeva",
+//         birthdate:"22-09-2002",
+//         faculty:"Applied Mathematic SABAH groups",
+//         occupation:"Front end developer",
+//         GPA:95,
+//         isMarried:false
+//     },
+//     {
+//         id:2,
+//         name:"Nigar",
+//         surname:"Naghiyeva",
+//         birthdate:"17-05-2002",
+//         faculty:"Chemistry",
+//         occupation:"teacher",
+//         GPA:95,
+//         isMarried:false
+//     },
+//     {
+//         id:3,
+//         name:"Gulnar",
+//         surname:"Naghiyeva",
+//         birthdate:"22-09-2002",
+//         faculty:"Applied Mathematic SABAH groups",
+//         occupation:"Front end developer",
+//         GPA:95,
+//         isMarried:false
+//     },
+//     {
+//         id:4,
+//         name:"Nigar",
+//         surname:"Naghiyeva",
+//         birthdate:"17-05-2002",
+//         faculty:"Chemistry",
+//         occupation:"teacher",
+//         GPA:95,
+//         isMarried:false
+//     },
+//     {
+//         id:5,
+//         name:"Gulnar",
+//         surname:"Naghiyeva",
+//         birthdate:"22-09-2002",
+//         faculty:"Applied Mathematic SABAH groups",
+//         occupation:"Front end developer",
+//         GPA:95,
+//         isMarried:false
+//     },
+//     {
+//         id:6,
+//         name:"Nigar",
+//         surname:"Naghiyeva",
+//         birthdate:"17-05-2002",
+//         faculty:"Chemistry",
+//         occupation:"teacher",
+//         GPA:95,
+//         isMarried:false
+//     },
+//     {
+//         id:7,
+//         name:"Gulnar",
+//         surname:"Naghiyeva",
+//         birthdate:"22-09-2002",
+//         faculty:"Applied Mathematic SABAH groups",
+//         occupation:"Front end developer",
+//         GPA:95,
+//         isMarried:false
+//     },
+//     {
+//         id:8,
+//         name:"Nigar",
+//         surname:"Naghiyeva",
+//         birthdate:"17-05-2002",
+//         faculty:"Chemistry",
+//         occupation:"teacher",
+//         GPA:95,
+//         isMarried:false
+//     },
+//     {
+//         id:9,
+//         name:"Gulnar",
+//         surname:"Naghiyeva",
+//         birthdate:"22-09-2002",
+//         faculty:"Applied Mathematic SABAH groups",
+//         occupation:"Front end developer",
+//         GPA:95,
+//         isMarried:false
+//     },
+//     {
+//         id:10,
+//         name:"Nigar",
+//         surname:"Naghiyeva",
+//         birthdate:"17-05-2002",
+//         faculty:"Chemistry",
+//         occupation:"teacher",
+//         GPA:95,
+//         isMarried:false
+//     },
+//     {
+//         id:11,
+//         name:"Gulnar",
+//         surname:"Naghiyeva",
+//         birthdate:"22-09-2002",
+//         faculty:"Applied Mathematic SABAH groups",
+//         occupation:"Front end developer",
+//         GPA:95,
+//         isMarried:false
+//     },
+//     {
+//         id:12,
+//         name:"Nigar",
+//         surname:"Naghiyeva",
+//         birthdate:"17-05-2002",
+//         faculty:"Chemistry",
+//         occupation:"teacher",
+//         GPA:95,
+//         isMarried:false
+//     },
+//     {
+//         id:13,
+//         name:"Gulnar",
+//         surname:"Naghiyeva",
+//         birthdate:"22-09-2002",
+//         faculty:"Applied Mathematic SABAH groups",
+//         occupation:"Front end developer",
+//         GPA:95,
+//         isMarried:false
+//     },
+//     {
+//         id:14,
+//         name:"Nigar",
+//         surname:"Naghiyeva",
+//         birthdate:"17-05-2002",
+//         faculty:"Chemistry",
+//         occupation:"teacher",
+//         GPA:95,
+//         isMarried:false
+//     },
+//     {
+//         id:15,
+//         name:"Gulnar",
+//         surname:"Naghiyeva",
+//         birthdate:"22-09-2002",
+//         faculty:"Applied Mathematic SABAH groups",
+//         occupation:"Front end developer",
+//         GPA:95,
+//         isMarried:false
+//     },
+//     {
+//         id:16,
+//         name:"Nigar",
+//         surname:"Naghiyeva",
+//         birthdate:"17-05-2002",
+//         faculty:"Chemistry",
+//         occupation:"teacher",
+//         GPA:95,
+//         isMarried:false
+//     },
     
-]
+// ]
 
 
 app.get('/api', (req, res) => {
@@ -180,55 +198,47 @@ app.get('/api', (req, res) => {
 })
 
 //get all stundent
-app.get("/api/students",(req,res)=>{
-    res.send({
-        data:STUDENTS,
-        message:'data get succesfully'
-    })
-})
-// app.get("/students/paginate", paginatedResults(STUDENTS), (req, res) => {
-//     res.json(res.paginatedResults);
-//   });
+app.get("/api/students",async (req,res)=>{
+    const {name} = req.query;
+    const students = await StudentModel.find();
+    let page = req.query.page
+    let limit = req.query.limit
+    if(page&&limit){
+        let start = (page-1)*limit
+        let end = (page*limit)
+        res.send({
+            totalCount:students.length,
+            totalPage:Math.ceil(students.length/limit),
+            data:students.slice(start,end)
+        })
+    }
 
-// function paginatedResults(model) {
-//     // middleware function
-//     return (req, res, next) => {
-//       const page = parseInt(req.query.page);
-//       const limit = parseInt(req.query.limit);
+    if(name===undefined){
+       res.status(200).send({
+        data:students,
+        message:"data get success"
+       })
+    }
+    
    
-//       // calculating the starting and ending index
-//       const startIndex = (page - 1) * limit;
-//       const endIndex = page * limit;
-   
-//       const results = {};
-//       if (endIndex < model.length) {
-//         results.next = {
-//           page: page + 1,
-//           limit: limit
-//         };
-//       }
-   
-//       if (startIndex > 0) {
-//         results.previous = {
-//           page: page - 1,
-//           limit: limit
-//         };
-//       }
-   
-//       results.results = model.slice(startIndex, endIndex);
-   
-//       res.paginatedResults = results;
-//       next();
-//     };
-//   }
+    else{
+        res.status(200).send({
+            data:students.filter((x)=>x.name.toLowerCase().trim().includes(name.toLowerCase().trim())),
+            message:"data get success"
+        })
+    }
+  
+});
+
 
 //get student by id
-app.get("/api/students/:id",(req,res)=>{
+app.get("/api/students/:id",async(req,res)=>{
     const id = req.params.id
-    const student = STUDENTS.find((x)=>x.id==id)
+    const student = await StudentModel.findById(id);
+    console.log('student found', student);
     if(!student){
         res.status(204).send("student not found")
-    }
+    } 
     else{
         res.status(200).send({
             data:student,
@@ -238,15 +248,13 @@ app.get("/api/students/:id",(req,res)=>{
 })
 
 //delete student
-app.delete("/api/students/:id",(req,res)=>{
+app.delete("/api/students/:id", async(req,res)=>{
     const id = req.params.id
-    const student = STUDENTS.find((x)=>x.id==id)
-   
+    const student = await StudentModel.findByIdAndDelete(id);
+       
     if(student===undefined){
         res.status(404).send("student not found")
     }else{
-        const index = STUDENTS.indexOf(student)
-        STUDENTS.splice(index,1)
         res.status(203).send({
             data:student,
             message:"student succesfully deleted"
@@ -255,11 +263,10 @@ app.delete("/api/students/:id",(req,res)=>{
 })
 
 //post
-app.post("/api/students",(req,res)=>{
+app.post("/api/students", async(req,res)=>{
     const{name,surname,birthdate,faculty,occupation,isMarried,GPA} = req.body
     if(name!=="" || surname!==""){
-        const newStudent = {
-            id: crypto.randomUUID(),
+        const newStudent = new StudentModel({
             name: name,
             surname: surname,
             birthdate:birthdate,
@@ -267,10 +274,11 @@ app.post("/api/students",(req,res)=>{
             occupation:occupation,
             isMarried:isMarried,
             GPA:GPA
-          };
+          });
 
-          STUDENTS.push(newStudent);
-          res.status(201).send("created succesfully");
+        //   STUDENTS.push(newStudent);
+        await newStudent.save();
+        res.status(201).send("created succesfully");
     }
     else{
         res.status(404).send("created not succesfully");
@@ -278,7 +286,8 @@ app.post("/api/students",(req,res)=>{
     });
 
 //edit put
-app.put("/api/students/:id", (req, res) => {
+//put u dersde hele yazmamisiq
+app.put("/api/students/:id",(req, res) => {
     const id = req.params.id;
     const{name,surname,birthdate,faculty,occupation,isMarried,GPA} = req.body
     const existedStudent = STUDENTS.find((x) => x.id == id);
